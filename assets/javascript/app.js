86d476b76da75de3437a5fcd7e96554cb25a0f31
elaintran <elain.ttran@gmail.com>
2019-06-17 14:28:09 -0500

    empty array for featured locations

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -67,9 +67,8 @@ $('.dropdown').on('click', '.dropdown-item', function(event) {
             ""
             break;
     }
-    queryURL = "https://api.seatgeek.com/2/events?&lat=" + latitude + "&lon=" + longitude + "&client_id=" + clientID + "&per_page=12&taxonomies.name=" + categoryFilter;
-
     eventsArray = [];
+    queryURL = "https://api.seatgeek.com/2/events?&lat=" + latitude + "&lon=" + longitude + "&client_id=" + clientID + "&per_page=12&taxonomies.name=" + categoryFilter;
     seatGeek(queryURL);
 })
 
